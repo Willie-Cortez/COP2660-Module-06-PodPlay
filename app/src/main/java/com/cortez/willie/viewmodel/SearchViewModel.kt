c2366a00d7ef90d17bd010af2a4060b9755ad7ad
williecortez <w21c@gmail.com>
2021-04-20 09:09:26 -0400

    finished chapter 21

---
+++ app/src/main/java/com/cortez/willie/viewmodel/SearchViewModel.kt
@@ -0,0 +1,42 @@
+package com.cortez.willie.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.cortez.willie.repository.ItunesRepo
+import com.cortez.willie.service.PodcastResponse
+import com.cortez.willie.util.DateUtils
+
+class SearchViewModel(application: Application) : AndroidViewModel(application) {
+
+    var iTunesRepo: ItunesRepo? = null
+
+    data class PodcastSummaryViewData(
+        var name: String? = "",
+        var lastUpdated: String? = "",
+        var imageUrl: String? = "",
+        var feedUrl: String? = "")
+
+    private fun itunesPodcastToPodcastSummaryView(
+        itunesPodcast: PodcastResponse.ItunesPodcast):
+            PodcastSummaryViewData {
+        return PodcastSummaryViewData(
+            itunesPodcast.collectionCensoredName,
+            DateUtils.jsonDateToShortDate(itunesPodcast.releaseDate),
+            itunesPodcast.artworkUrl30,
+            itunesPodcast.feedUrl)
+    }
+
+    suspend fun searchPodcasts(term: String): List<PodcastSummaryViewData> {
+        val results = iTunesRepo?.searchByTerm(term)
+
+        if (results != null && results.isSuccessful) {
+            val podcasts = results.body()?.results
+            if (!podcasts.isNullOrEmpty()) {
+                return podcasts.map { podcast ->
+                    itunesPodcastToPodcastSummaryView(podcast)
+                }
+            }
+        }
+        return emptyList()
+    }
+}
